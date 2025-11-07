2a9e80d9299f049280a14daeb19d7289c34a008c
DennyBeus <dennis.gladkov@gmail.com>
2025-11-07 18:47:11 +0300

    added try-except, new tasks vision and upgrade del_task func

--- main.py
+++ main.py
@@ -9,10 +9,13 @@ def status_task(status):
     tasks[status]['done'] = change_status
 
 def del_task(number):
-    del tasks[number]
-    for i in range(len(tasks) + 1 - number):
-        tasks[number + i] = tasks[number + i + 1]
-    del tasks[len(tasks)]
+    if number == len(tasks):
+        del tasks[number]
+    else:
+        del tasks[number]
+        for i in range(len(tasks) + 1 - number):
+            tasks[number + i] = tasks[number + i + 1]
+        del tasks[len(tasks)]
 
 tasks = {}
 
@@ -25,24 +28,15 @@ filter_mode = 'all'
 while True:
     print('Менеджер задач:')
     print('-' * 56)
-    if filter_mode == 'active': 
-        for i in range(len(tasks)):
-            if tasks[i + 1]['done'] == False:
-                print(f'{i + 1} - [ ] - {tasks[i + 1]['task']}')
-    if filter_mode == 'done':
-        for i in range(len(tasks)):
-            if tasks[i + 1]['done'] == True:
-                print(f'{i + 1} - [✓] - {tasks[i + 1]['task']}')
-    if filter_mode == 'all':
-        if tasks == {}:
-            print('У вас пока нет задач')
-        else:
-            for i in range(len(tasks)):
-                if tasks[i + 1]['done'] == True:
-                    symbol = '✓'
-                else:
-                    symbol = ' '
-                print(f'{i + 1} - [{symbol}] - {tasks[i + 1]['task']}')
+    if tasks == {}:
+        print('Список задач пуст')
+    for i in range(len(tasks)):
+        if filter_mode == 'active' and tasks[i + 1]['done']:
+            continue
+        if filter_mode == 'done' and not tasks[i + 1]['done']:
+            continue
+        symbol = '✓' if tasks[i + 1]['done'] else ' '
+        print(f'{i + 1} - [{symbol}] - {tasks[i + 1]['task']}')
     print('-' * 56)
     for row in menu_buttons:
         for elem in row:
@@ -50,23 +44,40 @@ while True:
         print()
     print('Напишите "exit" для выхода из программы')
     answer = input()
-    if int(answer) == 1:
-        task_text = input('Опишите задачу: ')
-        add_task(task_text)
-    if int(answer) == 2:
-        status = int(input('Укажите номер задачи: '))
-        status_task(status)
-    if int(answer) == 3:
-        number = int(input('Укажите номер задачи: '))
-        del_task(number)
-    if int(answer) == 4:
-        filter_mode = 'all'
-    if int(answer) == 5:
-        filter_mode = 'active'
-    if int(answer) == 6:
-        filter_mode = 'done'
     if answer == 'exit':
         break
+    try:
+        answer_num = int(answer)
+    except ValueError:
+        print('* Введите число! *')
+        continue
+    if answer_num == 1:
+        task_text = input('Опишите задачу: ')
+        add_task(task_text)
+    if answer_num == 2:
+        try:
+            status = int(input('Укажите номер задачи: '))
+            if status > len(tasks):
+                print('* Задачи с таким номером нет! *')
+                continue
+        except ValueError:
+            print('* Введите число! *')
+            continue
+        status_task(status)
+    if answer_num == 3:
+        try:
+            number = int(input('Укажите номер задачи: '))
+            if number > len(tasks):
+                print('* Задачи с таким номером нет! *')
+                continue
+        except ValueError:
+            print('* Введите число! *')
+            continue
+        del_task(number)
+    if answer_num == 4:
+        filter_mode = 'all'
+    if answer_num == 5:
+        filter_mode = 'active'
+    if answer_num == 6:
+        filter_mode = 'done'
         
-
-
